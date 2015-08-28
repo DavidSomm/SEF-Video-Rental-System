ba92a9d3680b694c73a90a51d44b98aa970f5879
Jack Della <jacko1394@gmail.com>
2015-08-29 00:50:40 +1000

    code update

--- SEF-Video-Rental-System/src/customer/DB_Customer.java
+++ SEF-Video-Rental-System/src/customer/DB_Customer.java
@@ -1,11 +1,20 @@
 package customer;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class DB_Customer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	private static ArrayList<Customer> customerDatabase = new ArrayList<Customer>();
+	
+	
 	public DB_Customer() {
+		
+		
+		
+		
+		
 		
 	}
 
